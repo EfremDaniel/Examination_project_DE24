07bc43df5e5c05585a2bb01fe57dac455bf1ece7
Efrem <Efremdaniel27@gmail.com>
2025-12-16 15:03:49 +0100

    setup for vehicle data

---
+++ src/data_transform/models/src/src_trafikanalys.sql
@@ -0,0 +1,13 @@
+with 
+tr as (select * from {{ source('trafikanalys', 'trafik_analys') }})
+
+select
+    AR as 'year',
+    Drivmedel as fuel,
+    antal_i_trafik as car_in_traffic 
+from tr
+where 
+    AR between 2016 and 2024
+    and Drivmedel in ['El', 'Elhybrid', 'Laddhybrid']
+order by
+    AR desc;
